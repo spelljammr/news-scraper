fa38518887a94ace6ac8463819222aff1dd98558
ahydorn <adamhydorn@gmail.com>
2019-07-23 16:03:55 -0700

    replace + with backticks

--- server.js
+++ server.js
@@ -106,5 +106,5 @@ app.post("/articles/:id", function(req, res) {
 });
 // Start the server
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+    console.log(`App running on port ${PORT}`);
 });
