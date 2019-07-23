3807c54fb6ac77be59f35011e1c2ee55f398e957
ahydorn <adamhydorn@gmail.com>
2019-07-23 16:47:39 -0700

    fixed get request error

--- server.js
+++ server.js
@@ -21,42 +21,41 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Connect to MongoDB
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhostmongoHeadlines';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
 mongoose.connect(MONGODB_URI);
 
 // A GET route for scraping the news website.
-// First, we grab the body of the html with axios
-axios.get('https://www.reddit.com/').then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+app.get("/scrape", function(req, res) {
+    // First, we grab the body of the html with axios
+    axios.get('https://www.reddit.com/').then(function(response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+        // Now, we grab every h2 within an article tag, and do the following:
+        $('article h2').each(function(i, element) {
+            // Save an empty result object
+            var result = {};
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this)
+                .children('a')
+                .text();
+            result.link = $(this)
+                .children('a')
+                .attr('href');
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    // Log any errors
+                    console.log(err);
+                });
+        });
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $('article h2').each(function(i, element) {
-        // Save an empty result object
-        var result = {};
-
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-            .children('a')
-            .text();
-        result.link = $(this)
-            .children('a')
-            .attr('href');
-
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-            .then(function(dbArticle) {
-                // View the added result in the console
-                console.log(dbArticle);
-            })
-            .catch(function(err) {
-                // Log any errors
-                console.log(err);
-            });
+        // Send a message to the client
+        res.send('Scrape Finished');
     });
-
-    // Send a message to the client
-    res.send('Scrape Finished');
 });
 
 
@@ -74,11 +73,11 @@ app.get('/articles', function(req, res) {
         });
 });
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
+app.get('/articles/:id', function(req, res) {
     // Prepare a query to find article by id.
     db.Article.findOne({ _id: req.params.id })
         // Populate with related notes
-        .populate("note")
+        .populate('note')
         .then(function(dbArticle) {
             // Send back results of successful query to client
             res.json(dbArticle);
