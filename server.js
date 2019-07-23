7c9cf6199dc82e139587dcca7e2915c6ab38479c
ahydorn <adamhydorn@gmail.com>
2019-07-23 15:33:06 -0700

    added listener

--- server.js
+++ server.js
@@ -14,7 +14,18 @@ const PORT = process.env.PORT || 3000;
 // Init express
 const app = express();
 
+// Middleware configuration
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// Connect to MongoDB
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhostmongoHeadlines';
+mongoose.connect(MONGODB_URI);
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
