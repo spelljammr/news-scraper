24a8847e7f982dc99cfb6c87190322ac1f9e4d32
ahydorn <adamhydorn@gmail.com>
2019-07-23 17:17:59 -0700

    many fixes

--- models/Article.js
+++ models/Article.js
@@ -1,4 +1,4 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
@@ -18,12 +18,12 @@ var ArticleSchema = new Schema({
     //  Object used to gather note id.
     note: {
         type: Schema.Types.ObjectId,
-        ref: "Note"
+        ref: 'Note'
     }
 });
 
 // Create model using Mongoose's method.
-var Article = mongoose.model("Article", ArticleSchema);
+var Article = mongoose.model('Article', ArticleSchema);
 
 // Export the Article model to be used elsewhere.
 module.exports = Article;
